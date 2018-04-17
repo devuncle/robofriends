6e99693d59d78c61027cb709113952dea3c27813
devuncle <devuncle.com@gmail.com>
2018-04-17 14:46:44 +0800

    biulding app 1

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const Card = () => {
-    return ( 
-      <div>
-        <img src="https://robohash.org/test" alt="robots" />
+const Card = (props) => {
+    return (
+      // <h1>RoboFriends</h1> 
+      <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' >
+        <img src={"https://robohash.org/${props.id}?200x200"} alt="robots" />
         <div>
-          <h2>Jane Doe</h2>
-          <p>jane.doe@gmail.com</p>
+          <h2>{props.name}</h2>
+          <p>{props.email}</p>
 
         </div>
       </div>
---
+++ src/robots.js
@@ -0,0 +1,57 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 2,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 3,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 4,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 5,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 6,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 7,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 8,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    },
+    {
+        id: 9,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.com'
+    }
+
+]
