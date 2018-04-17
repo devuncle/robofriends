81ed84b25b2b78d4a69151979251b2ac12ec671f
devuncle <devuncle.com@gmail.com>
2018-04-17 20:36:56 +0800

    react app 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import {robots} from './robots';
-import CardList from './CardList'
 
-ReactDOM.render( <CardList robots={robots}/> , document.getElementById('root'));
+
+ReactDOM.render( <App /> , document.getElementById('root'));
 registerServiceWorker();
