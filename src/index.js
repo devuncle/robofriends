74e6d08524da0bb849bf9f01b2039fc68512574c
devuncle <devuncle.com@gmail.com>
2018-04-17 19:16:36 +0800

    finish react app 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import {robots} from './robots';
 import CardList from './CardList'
 
-ReactDOM.render( <CardList robots={robots}/> 
-, document.getElementById('root'));
+ReactDOM.render( <CardList robots={robots}/> , document.getElementById('root'));
 registerServiceWorker();
